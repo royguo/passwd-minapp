f6982efde6be997089ce240cf8b6dd7e124aae24
Roy Guo <royguo1988@gmail.com>
2018-08-26 23:24:18 +0800

    #

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,14 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+    Length: 4,        //输入框个数
+    isFocus: true,    //聚焦
+    Value: "",        //输入的内容
+    ispassword: false, //是否密文显示 true为密文， false为明文。
   },
   //事件处理函数
   bindViewTap: function() {
@@ -50,5 +54,22 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  Focus(e) {
+    var that = this;
+    console.log(e.detail.value);
+    var inputValue = e.detail.value;
+    that.setData({
+      Value: inputValue,
+    })
+  },
+  Tap() {
+    var that = this;
+    that.setData({
+      isFocus: true,
+    })
+  },
+  formSubmit(e) {
+    console.log(e.detail.value.password);
+  },
 })
